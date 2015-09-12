894816ecc3b12ed77f123c5e1db67d87b988332e
Dawei Xia <xiadw_public@163.com>
2015-09-12 12:10:21 +0800

    Add bio JSON without display function

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,18 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+	"name": "Dawei Xia",
+	"role": "Teacher & Web Developer",
+	"contacts": {
+		"mobile": "15555555555",
+		"email": "example@example.com",
+		"github": "https://github.com/DaweiXia",
+		"twitter": "I don't use twitter",
+		"location": "Shenyang China"
+	},
+	"welcomMessage": "Welcom to my site! I am Dawei!",
+	"skills": ["Python", "HTML", "CSS", "JS", "JQuery"],
+	"biopic": "https://avatars1.githubusercontent.com/u/5789704?v=3&s=460",
+	"display": function() {}
+};
